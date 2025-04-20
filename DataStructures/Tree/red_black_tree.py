620c2f4c0df65cb5f996e95ae668e47d96f6bef8
tomasalvaradoarr <t.alvarado@unaides.edu.co>
2025-04-19 23:53:45 -0500

    AVANZE 2.0

--- DataStructures/Tree/red_black_tree.py
+++ DataStructures/Tree/red_black_tree.py
@@ -20,7 +20,6 @@ def put (my_bst, key, value):
     return my_bst
 
 
-
 def get_node (root, key):
     
     if root is None:
@@ -135,3 +134,248 @@ def value_set (my_rbt):
         value_list = key_set_tree(my_rbt["root"], value_list) 
     return value_list
         
+        
+def get_min_node (root):
+    
+    if root is None:
+        return None
+    else:
+        return get_min_node(root["left"])  # busca recursivamente el menor de los hijos izquierdos
+
+def get_min (my_rbt):
+    
+    return get_min_node(my_rbt["root"]) # se llama a la funcion que busca el menor de los hijos izquierdos
+
+
+def get_max_node (root):
+    
+    if root is None:
+        return None
+    else:
+        return get_max_node(root["right"])
+    
+
+def get_max (my_rbt):
+    return get_max_node(my_rbt["root"]) # se llama a la funcion que busca el mayor de los hijos derechos
+
+def delete_min_node (root):
+    
+    if root is None:
+        return None
+   
+    if root["left"] is None:  # si ya el left no tiene hijos significa que es el menor 
+        return root["right"]  # En la llamada recursiva de arriba, ese valor se asignará al puntero izquierdo de su padre, “saltándonos” a root y dejando intactos todos los nodos mayores.
+    
+    root["left"] = delete_min_node(root["left"]) # si tiene hijo izquierdo se llama a la funcion recursivamente
+    
+    return root         
+
+def delete_min (my_rbt):
+    
+    my_rbt["root"] = delete_min_node(my_rbt["root"]) # se llama a la funcion que elimina el menor de los hijos izquierdos
+    if my_rbt["root"] is not None:
+        my_rbt["root"]["color"] = "BLACK" # se cambia el color del root 
+        
+
+def delete_max_node (root):
+    if root is None:
+        return None
+    
+    if root["right"] is None:
+        return root["left"]
+    
+    root["right"] = delete_max_node(root["right"])
+    
+    return root 
+
+def delete_max (my_rbt):
+    
+    my_rbt["root"] = delete_max_node(my_rbt["root"]) 
+    if my_rbt["root"] is not None:
+        my_rbt["root"]["color"] = "BLACK" 
+    
+            
+def floor_key (root, key):
+    
+    if root is None:
+        return None
+    if key == root["key"]: # si la llave ya esta retorna la llave
+        return root["key"]
+    if key < root["key"]:
+        return floor_key(root["left"], key) # si la llave es menor el candidato debe estar a la izquierda
+    
+    candidate = floor_key(root["right"], key) # si la llave es mayor el candidato debe estar a la derecha
+    if candidate is not None:  
+        return candidate    # si el candidato no es none significa que hay algo a la derecha y se retorna la llave del candidato
+    else:
+        return root["key"] # si el candidato es none significa que no hay nada a la derecha y se retorna la llave del padre
+         
+        
+def floor (my_rbt, key):
+    
+    x = floor_key(my_rbt["root"], key) # se llama a la funcion que busca el menor de los hijos izquierdos
+    
+    return x 
+
+def ceiling_key (root, key):
+    
+    if root is None:
+        return None
+    if key == root["key"]:
+        return root["key"]
+    
+    if key > root["key"]:
+        return ceiling_key(root["right"], key)
+    
+    
+    candidate = ceiling_key(root["left"], key) # aqui es key < root["key"] por lo que ya estaria a la izquierda
+    
+    if candidate is not None:
+        return candidate 
+    else:
+        return root["key"]
+    
+
+def ceiling (my_rbt, key):
+    
+    x = ceiling_key(my_rbt["root"], key) 
+    return x
+    
+"""def select_key (root, key):
+    
+def select (my_rbt, key):"""
+    
+    
+def rank_keys (root, key):
+    
+    if root is None:
+        return 0
+    if key <= root["key"]:
+        return rank_keys(root["left"], key)
+    else:
+        menores = size_tree(root["left"]) 
+        
+        return 1 + menores + rank_keys(root["right"], key)
+    
+    
+def rank (my_rbt, key):
+    
+    x = rank_keys(my_rbt["root"], key) 
+    return 
+      
+      
+def height_tree (root):
+    
+    if root is None:
+        return 0 
+    else:
+        x = height_tree(root["left"]) # hace recusrivamente el conteo de izquierda
+        y = height_tree(root["right"]) # y derecha 
+        
+        if x >= y:
+            return x + 1 # se le suma 1 al mayor de los dos (1 porque es el root)
+        elif y > x:
+            return y +1
+       
+
+def height (my_rbt):
+    return height_tree(my_rbt["root"])
+
+def values_range(root, key_initial, key_final, value_list):
+    
+    if root is None:
+        return value_list
+    if key_initial < root["key"]: # si la llave inicial es menor que la del root se tiene que ir a la izquierda
+        values_range(root["left"], key_initial, key_final, value_list)
+    if key_initial <= root["key"] <= key_final:  # si esta en el rango entra a la lista
+        sll.add_last(value_list, root["value"])
+    if key_final > root["key"]: # si la llave final es mayor que la del root se tiene que ir a la derecha 
+        values_range(root["right"], key_initial, key_final, value_list)
+    
+    return value_list
+    
+def values (my_rbt, key_initial, key_final):
+    
+    value_list = sll.new_list()
+    if my_rbt is not None:
+        value_list = values_range(my_rbt["root"], key_initial, key_final, value_list) 
+    return value_list
+
+        
+    
+     
+def keys_range(root, key_initial, key_final, key_list):
+    if root is None:
+        return key_list
+
+    if key_initial < root["key"]:
+        keys_range(root["left"], key_initial, key_final, key_list)
+
+    if key_initial <= root["key"] <= key_final:
+        sll.add_last(key_list, root["key"])
+
+    if root["key"] < key_final:
+        keys_range(root["right"], key_initial, key_final, key_list)
+
+    return key_list
+
+        
+
+def keys (my_rbt, key_initial, key_final):
+    
+    key_list = sll.new_list()
+    if my_rbt is not None:
+        key_list = values_range(my_rbt["root"], key_initial, key_final, key_list) 
+    return key_list
+
+
+def rotate_left (node_rbt):
+    
+    x= node_rbt["right"]            # se guarda el hijo derecho
+    node_rbt["right"] = x["left"]   # se cambia el hijo derecho por el hijo izquierdo del hijo derecho
+    x["left"] = node_rbt            # se cambia el hijo izquierdo del hijo derecho por el nodo padre
+    x["color"] = x["left"]["color"] # se cambia el color del nodo hijo por el del padre
+    x["left"]["color"] = "RED"   # se cambia el color del padre por rojo
+    x["size"] = node_rbt["size"]
+    node_rbt ["size"] = 1 + size_tree(node_rbt["left"]) + size_tree(node_rbt["right"]) # se cambia el tamaño del padre
+    
+    return x
+
+
+def rotate_right (node_rbt):
+    
+    x= node_rbt["left"] # se guarda el hijo izquierdo
+    node_rbt["left"] = x["right"] # se cambia el hijo izquierdo por el hijo derecho del hijo izquierdo
+    x["right"] = node_rbt # se cambia el hijo derecho del hijo izquierdo por el nodo padre
+    x["color"] = x["right"]["color"]
+    x["right"]["color"] = "RED"  
+    x["size"] = node_rbt["size"]
+    node_rbt ["size"] = 1 + size_tree(node_rbt["left"]) + size_tree(node_rbt["right"]) \
+        
+    return x 
+
+def flip_node_color (node_rbt):
+    
+    if node_rbt is not None:
+        
+        if node_rbt["color"] == "RED":
+            node_rbt["color"] = "BLACK"
+        else:
+            node_rbt["color"] = "RED"
+            
+
+def flip_colors (node_rbt):
+    
+    if node_rbt is not None:
+        flip_node_color(node_rbt["left"])
+        flip_node_color(node_rbt["right"])
+        flip_node_color(node_rbt)
+    
+    
+def is_red (node_rbt):
+    
+    if node_rbt is not None:
+        return node_rbt["color"] == "RED"
+    else:
+        return False
+    
